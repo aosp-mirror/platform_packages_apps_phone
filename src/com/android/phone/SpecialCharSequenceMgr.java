6fc6603ae72f8a2430beddee2464310a0a97a958
Tracy Pu <tracypu@motorola.com>
2009-07-02 11:55:10 -0400

    Change hidden menu pop-up title to MEID for CDMA
    - When user keys in *#06# through the dialpad, the pop-up screen title
      will be IMEI on GSM network and MEID on CDMA network
    - During a call, when user launches the dialpad through the in-call
      option menu to add call, then, keys in *#06#, the pop-up screen title
      will be IMEI on GSM network and MEID on CDMA network
    
    	modified:   res/values/strings.xml
    	modified:   src/com/android/phone/SpecialCharSequenceMgr.java

--- src/com/android/phone/SpecialCharSequenceMgr.java
+++ src/com/android/phone/SpecialCharSequenceMgr.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.provider.Telephony.Intents;
 import com.android.internal.telephony.PhoneFactory;
+import com.android.internal.telephony.Phone;
 import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 import android.view.WindowManager;
@@ -172,8 +173,13 @@ public class SpecialCharSequenceMgr {
     static boolean handleIMEIDisplay(Context context,
                                      String input, boolean useSystemWindow) {
         if (input.equals(MMI_IMEI_DISPLAY)) {
-            showIMEIPanel(context, useSystemWindow);
-            return true;
+            if (PhoneApp.getInstance().phone.getPhoneName().equals("CDMA")) {
+                showMEIDPanel(context, useSystemWindow);
+                return true;
+            } else {
+                showIMEIPanel(context, useSystemWindow);
+                return true;
+            }
         }
 
         return false;
@@ -187,6 +193,20 @@ public class SpecialCharSequenceMgr {
         AlertDialog alert = new AlertDialog.Builder(context)
                 .setTitle(R.string.imei)
                 .setMessage(imeiStr)
+                .setPositiveButton(R.string.ok, null)
+                .setCancelable(false)
+                .show();
+        alert.getWindow().setType(WindowManager.LayoutParams.TYPE_PRIORITY_PHONE);
+    }
+
+    static void showMEIDPanel(Context context, boolean useSystemWindow) {
+        if (DBG) log("showMEIDPanel");
+
+        String meidStr = PhoneFactory.getDefaultPhone().getDeviceId();
+
+        AlertDialog alert = new AlertDialog.Builder(context)
+                .setTitle(R.string.meid)
+                .setMessage(meidStr)
                 .setPositiveButton(R.string.ok, null)
                 .setCancelable(false)
                 .show();
