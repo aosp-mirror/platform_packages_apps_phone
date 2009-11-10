c883940782f6e34852a327ee7068529d4f64b482
Nicolas Catania <niko@google.com>
2009-11-10 09:44:20 -0800

    Optimized version of the ButtonGridLayout.
        New mButtons field to cache the children (buttons).
        Use local variable to cache fields values in the loops.
        onMeasure, added missing super.onMeasure call.

--- src/com/android/phone/ButtonGridLayout.java
+++ src/com/android/phone/ButtonGridLayout.java
@@ -18,6 +18,7 @@ package com.android.phone;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View.MeasureSpec;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,13 +43,34 @@ import android.view.ViewGroup;
  */
 // TODO: Blindly layout the buttons w/o checking if we overrun the
 // bottom-right corner.
-// TODO: This class and the one in the Contacts app are almost
-// duplicates. The Contacts instance does not have a
-// setChildrenBackground method.
 
 public class ButtonGridLayout extends ViewGroup {
-    private final int COLUMNS = 3;
-    private final int ROWS = 4;
+    static private final String TAG = "ButtonGridLayout";
+    static private final int COLUMNS = 3;
+    static private final int ROWS = 4;
+    static private final int NUM_CHILDREN = ROWS * COLUMNS;
+
+    private View[] mButtons = new View[NUM_CHILDREN];
+
+    // This what the fields represent (height is similar):
+    // PL: mPaddingLeft
+    // BW: mButtonWidth
+    // PR: mPaddingRight
+    //
+    //        mWidthInc
+    // <-------------------->
+    //   PL      BW      PR
+    // <----><--------><---->
+    //        --------
+    //       |        |
+    //       | button |
+    //       |        |
+    //        --------
+    //
+    // We assume mPaddingLeft == mPaddingRight == 1/2 padding between
+    // buttons.
+    //
+    // mWidth == COLUMNS x mWidthInc
 
     // Width and height of a button
     private int mButtonWidth;
@@ -60,7 +82,9 @@ public class ButtonGridLayout extends ViewGroup {
 
     // Height of the dialpad. Used to align it at the bottom of the
     // view.
+    private int mWidth;
     private int mHeight;
+
 
     public ButtonGridLayout(Context context) {
         super(context);
@@ -75,59 +99,79 @@ public class ButtonGridLayout extends ViewGroup {
     }
 
     /**
-     * Set the background of all the children. Typically a selector to
-     * change the background based on some combination of the button's
-     * attributes (e.g pressed, enabled...)
-     * @param background Is a resource id to be used for each button's background.
+     * Cache the buttons in a member array for faster access.  Compute
+     * the measurements for the width/height of buttons.  The inflate
+     * sequence is called right after the constructor and before the
+     * measure/layout phase.
      */
-    public void setChildrenBackgroundResource(int resid) {
-        final int count = ROWS * COLUMNS;
-
-        for (int i = 0; i < count; i++) {
-            getChildAt(i).setBackgroundResource(resid);
-        }
-    }
-
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        int i = 0;
-        // The last row is bottom aligned.
-        int y = (b - t) - mHeight + mPaddingTop;
-        for (int row = 0; row < ROWS; row++) {
-            int x = mPaddingLeft;
-            for (int col = 0; col < COLUMNS; col++) {
-                View child = getChildAt(i);
-
-                child.layout(x, y, x + mButtonWidth, y + mButtonHeight);
-
-                x += mWidthInc;
-                i++;
-            }
-            y += mHeightInc;
-        }
-      }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        // Measure the first child and get it's size
-        View child = getChildAt(0);
-        child.measure(MeasureSpec.UNSPECIFIED , MeasureSpec.UNSPECIFIED);
-
-        // Make sure the other children are measured as well, to initialize
-        for (int i = 1; i < getChildCount(); i++) {
-            getChildAt(i).measure(MeasureSpec.UNSPECIFIED , MeasureSpec.UNSPECIFIED);
+    protected void onFinishInflate () {
+        super.onFinishInflate();
+        final View[] buttons = mButtons;
+        for (int i = 0; i < NUM_CHILDREN; i++) {
+            buttons[i] = getChildAt(i);
+            // Measure the button to get initialized.
+            buttons[i].measure(MeasureSpec.UNSPECIFIED , MeasureSpec.UNSPECIFIED);
         }
 
-        // Store these to be reused in onLayout.
+        // Cache the measurements.
+        final View child = buttons[0];
         mButtonWidth = child.getMeasuredWidth();
         mButtonHeight = child.getMeasuredHeight();
         mWidthInc = mButtonWidth + mPaddingLeft + mPaddingRight;
         mHeightInc = mButtonHeight + mPaddingTop + mPaddingBottom;
+        mWidth = COLUMNS * mWidthInc;
         mHeight = ROWS * mHeightInc;
+    }
 
-        final int width = resolveSize(COLUMNS * mWidthInc, widthMeasureSpec);
+    /**
+     * Set the background of all the children. Typically a selector to
+     * change the background based on some combination of the button's
+     * attributes (e.g pressed, enabled...)
+     * @param resid Is a resource id to be used for each button's background.
+     */
+    public void setChildrenBackgroundResource(int resid) {
+        final View[] buttons = mButtons;
+        for (int i = 0; i < NUM_CHILDREN; i++) {
+            buttons[i].setBackgroundResource(resid);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        final View[] buttons = mButtons;
+        final int paddingLeft = mPaddingLeft;
+        final int buttonWidth = mButtonWidth;
+        final int buttonHeight = mButtonHeight;
+        final int widthInc = mWidthInc;
+        final int heightInc = mHeightInc;
+
+        int i = 0;
+        // The last row is bottom aligned.
+        int y = (bottom - top) - mHeight + mPaddingTop;
+        for (int row = 0; row < ROWS; row++) {
+            int x = paddingLeft;
+            for (int col = 0; col < COLUMNS; col++) {
+                buttons[i].layout(x, y, x + buttonWidth, y + buttonHeight);
+                x += widthInc;
+                i++;
+            }
+            y += heightInc;
+        }
+    }
+
+    /**
+     * This method is called twice in practice. The first time both
+     * with and height are constraint by AT_MOST. The second time, the
+     * width is still AT_MOST and the height is EXACTLY. Either way
+     * the full width/height should be in mWidth and mHeight and we
+     * use 'resolveSize' to do the right thing.
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        final int width = resolveSize(mWidth, widthMeasureSpec);
         final int height = resolveSize(mHeight, heightMeasureSpec);
-
         setMeasuredDimension(width, height);
     }
 }
