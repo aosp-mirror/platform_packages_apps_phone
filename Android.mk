edff2c481388cb70ebd4ea930c18adcf69b33900
Nicolas Catania <niko@google.com>
2009-12-03 14:45:19 -0800

    New package with unit tests for the phone app.
    Added a new tests subdir with all the tests to be consistent
    with other apps.
    The top makefile will build the tests.
    
    Added a unitest class for the cnap handling.
    Currently has only one trivial test.

--- Android.mk
+++ Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := user
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += \
         src/com/android/phone/INetworkQueryService.aidl \
         src/com/android/phone/INetworkQueryServiceCallback.aidl
@@ -29,3 +29,6 @@ LOCAL_PACKAGE_NAME := Phone
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
+
+# Build the test package
+include $(call all-makefiles-under,$(LOCAL_PATH))
