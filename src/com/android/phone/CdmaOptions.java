759c769b57b105b6b28b487ef50e51ed8653cc55
Naveen Kalla <nkalla@codeaurora.org>
2010-03-10 08:13:23 -0800

    Add button to set CDMA subscription source.
    CDMA subscription source allows the user to choose between RUIM and NV
    modes for CDMA.

--- src/com/android/phone/CdmaOptions.java
+++ src/com/android/phone/CdmaOptions.java
@@ -17,9 +17,11 @@
 package com.android.phone;
 
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
+import android.text.TextUtils;
 
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneFactory;
@@ -30,8 +32,10 @@ import com.android.internal.telephony.PhoneFactory;
 public class CdmaOptions extends PreferenceActivity {
 
     private CdmaRoamingListPreference mButtonCdmaRoam;
+    private CdmaSubscriptionListPreference mButtonCdmaSubscription;
 
     private static final String BUTTON_CDMA_ROAMING_KEY = "cdma_roaming_mode_key";
+    private static final String BUTTON_CDMA_SUBSCRIPTION_KEY = "subscription_key";
 
     @Override
     protected void onCreate(Bundle icicle) {
@@ -40,11 +44,50 @@ public class CdmaOptions extends PreferenceActivity {
         addPreferencesFromResource(R.xml.cdma_options);
 
         PreferenceScreen prefSet = getPreferenceScreen();
-        mButtonCdmaRoam =
-                (CdmaRoamingListPreference) prefSet.findPreference(BUTTON_CDMA_ROAMING_KEY);
+        mButtonCdmaRoam = (CdmaRoamingListPreference)prefSet
+                .findPreference(BUTTON_CDMA_ROAMING_KEY);
+
+        mButtonCdmaSubscription = (CdmaSubscriptionListPreference)prefSet
+                .findPreference(BUTTON_CDMA_SUBSCRIPTION_KEY);
+    }
+
+    @Override
+    protected void onResume() {
         if (PhoneFactory.getDefaultPhone().getPhoneType() != Phone.PHONE_TYPE_CDMA) {
             mButtonCdmaRoam.setEnabled(false);
+            mButtonCdmaSubscription.setEnabled(false);
+        } else {
+            mButtonCdmaRoam.setEnabled(true);
+            if(deviceSupportsNvAndRuim()) {
+                mButtonCdmaSubscription.setEnabled(true);
+            } else {
+                mButtonCdmaSubscription.setEnabled(false);
+            }
         }
+        super.onResume();
+    }
+
+    private boolean deviceSupportsNvAndRuim() {
+        // retrieve the list of subscription types supported by device.
+        String subscriptionsSupported = SystemProperties.get("ril.subscription.types");
+        boolean nvSupported = false;
+        boolean ruimSupported = false;
+
+        if (!TextUtils.isEmpty(subscriptionsSupported)) {
+            // searches through the comma-separated list for a match for "NV"
+            // and "RUIM" to update nvSupported and ruimSupported.
+            for (String subscriptionType : subscriptionsSupported.split(",")) {
+                subscriptionType = subscriptionType.trim();
+                if (subscriptionType.equalsIgnoreCase("NV")) {
+                    nvSupported = true;
+                }
+                if (subscriptionType.equalsIgnoreCase("RUIM")) {
+                    ruimSupported = true;
+                }
+            }
+        }
+
+        return (nvSupported && ruimSupported);
     }
 
     @Override
