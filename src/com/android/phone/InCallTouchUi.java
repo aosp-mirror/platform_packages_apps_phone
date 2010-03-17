825ccf147f31df0e00c22b6d66a63f2761533fda
David Brown <dab@google.com>
2010-03-16 22:03:04 -0700

    Shrink hit targets of in-call buttons to reduce likelihood of false hits
    Background:
    
      On both Nexus One and Droid, even though those devices have prox sensors,
      we still get occasional reports of false button presses on the in-call
      screen.
    
      Anecdotal evidence (see bugs 2191882 and 2450136) suggests that these
      are mostly due to (a) unintentional touches near the edge of the screen
      while holding the device in your hand, or (b) false ear touches just
      before the prox sensor kicks in.
    
    The fix:
    
      To reduce the likelihood of both these cases, this change modifies some
      of the in-call buttons to have a smaller "hit target".  Touch events too
      close to the button edges are now ignored.
    
      This is implemented using a custom OnTouchListener; see the
      SmallerHitTargetTouchListener class.
    
    Tuning/tweaking note:
    
      We currently define the "smaller hit target" to be 50% of the width and
      80% of the height of the corresponding button.  (See comments in the
      code for more details.)  I did some quick-and-dirty usability testing
      and found that almost nobody had problems hitting the smaller target,
      and in fact only one person had *any* misses at all.
    
      We still ought to do more testing, though, and tweak the hit target size
      if necessary.  (Ideally I'd like to run a dogfood experiment to collect
      the exact x/y values of *all* in-call button touch events over a few
      days, to find out for sure how "accurate" people are in real usage.)
    
    Tested:
      - Used the emulator (where I could tell *exactly* where I was clicking)
        to confirm that the hit target is working as intended.
      - On a real Passion device, confirmed that "normal" clicks still work
        (by aiming roughly for the centers of the buttons) but that clicks
        right near the button edges are now ignored.
    
    Bug: 2191882 (Shrink in-call hit targets to reduce likelihood of "Large ears can
                  initiate hold at beginning of conversation" bug)
    Bug: 2450136 (accidental mute with add call)
    
    Change-Id: I2b0c31911e851802ed894acc7fdc6a420fb14a12

--- src/com/android/phone/InCallTouchUi.java
+++ src/com/android/phone/InCallTouchUi.java
@@ -22,6 +22,7 @@ import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -193,6 +194,25 @@ public class InCallTouchUi extends FrameLayout
         mCdmaMergeButtonContainer = mInCallControls.findViewById(R.id.cdmaMergeButtonContainer);
         mCdmaMergeButton = (ImageButton) mInCallControls.findViewById(R.id.cdmaMergeButton);
         mCdmaMergeButton.setOnClickListener(this);
+
+        // Add a custom OnTouchListener to manually shrink the "hit
+        // target" of some buttons.
+        // (We do this for a few specific buttons which are vulnerable to
+        // "false touches" because either (1) they're near the edge of the
+        // screen and might be unintentionally touched while holding the
+        // device in your hand, or (2) they're in the upper corners and might
+        // be touched by the user's ear before the prox sensor has a chance to
+        // kick in.)
+        View.OnTouchListener smallerHitTargetTouchListener = new SmallerHitTargetTouchListener();
+        mAddButton.setOnTouchListener(smallerHitTargetTouchListener);
+        mMergeButton.setOnTouchListener(smallerHitTargetTouchListener);
+        mDialpadButton.setOnTouchListener(smallerHitTargetTouchListener);
+        mBluetoothButton.setOnTouchListener(smallerHitTargetTouchListener);
+        mSpeakerButton.setOnTouchListener(smallerHitTargetTouchListener);
+        mHoldButton.setOnTouchListener(smallerHitTargetTouchListener);
+        mSwapButton.setOnTouchListener(smallerHitTargetTouchListener);
+        mCdmaMergeButton.setOnTouchListener(smallerHitTargetTouchListener);
+        mSpeakerButton.setOnTouchListener(smallerHitTargetTouchListener);
 
         // Icons we need to change dynamically.  (Most other icons are specified
         // directly in incall_touch_ui.xml.)
@@ -631,7 +651,8 @@ public class InCallTouchUi extends FrameLayout
                     hintColorResId = R.color.incall_textEnded;  // red
                     break;
                 default:
-                    Log.e(LOG_TAG, "onGrabbedStateChange: unexpected grabbedState: " + grabbedState);
+                    Log.e(LOG_TAG, "onGrabbedStateChange: unexpected grabbedState: "
+                          + grabbedState);
                     hintTextResId = 0;
                     hintColorResId = 0;
                     break;
@@ -640,6 +661,97 @@ public class InCallTouchUi extends FrameLayout
             // Tell the InCallScreen to update the CallCard and force the
             // screen to redraw.
             mInCallScreen.updateSlidingTabHint(hintTextResId, hintColorResId);
+        }
+    }
+
+
+    /**
+     * OnTouchListener used to shrink the "hit target" of some onscreen
+     * buttons.
+     */
+    class SmallerHitTargetTouchListener implements View.OnTouchListener {
+        /**
+         * Width of the allowable "hit target" as a percentage of
+         * the total width of this button.
+         */
+        private static final int HIT_TARGET_PERCENT_X = 50;
+
+        /**
+         * Height of the allowable "hit target" as a percentage of
+         * the total height of this button.
+         *
+         * This is larger than HIT_TARGET_PERCENT_X because some of
+         * the onscreen buttons are wide but not very tall and we don't
+         * want to make the vertical hit target *too* small.
+         */
+        private static final int HIT_TARGET_PERCENT_Y = 80;
+
+        // Size (percentage-wise) of the "edge" area that's *not* touch-sensitive.
+        private static final int X_EDGE = (100 - HIT_TARGET_PERCENT_X) / 2;
+        private static final int Y_EDGE = (100 - HIT_TARGET_PERCENT_Y) / 2;
+        // Min/max values (percentage-wise) of the touch-sensitive hit target.
+        private static final int X_HIT_MIN = X_EDGE;
+        private static final int X_HIT_MAX = 100 - X_EDGE;
+        private static final int Y_HIT_MIN = Y_EDGE;
+        private static final int Y_HIT_MAX = 100 - Y_EDGE;
+
+        // True if the most recent DOWN event was a "hit".
+        boolean mDownEventHit;
+
+        /**
+         * Called when a touch event is dispatched to a view. This allows listeners to
+         * get a chance to respond before the target view.
+         *
+         * @return True if the listener has consumed the event, false otherwise.
+         *         (In other words, we return true when the touch is *outside*
+         *         the "smaller hit target", which will prevent the actual
+         *         button from handling these events.)
+         */
+        public boolean onTouch(View v, MotionEvent event) {
+            // if (DBG) log("SmallerHitTargetTouchListener: " + v + ", event " + event);
+
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                // Note that event.getX() and event.getY() are already
+                // translated into the View's coordinates.  (In other words,
+                // "0,0" is a touch on the upper-left-most corner of the view.)
+                int touchX = (int) event.getX();
+                int touchY = (int) event.getY();
+
+                int viewWidth = v.getWidth();
+                int viewHeight = v.getHeight();
+
+                // Touch location as a percentage of the total button width or height.
+                int touchXPercent = (int) ((float) (touchX * 100) / (float) viewWidth);
+                int touchYPercent = (int) ((float) (touchY * 100) / (float) viewHeight);
+                // if (DBG) log("- percentage:  x = " + touchXPercent + ",  y = " + touchYPercent);
+
+                // TODO: user research: add event logging here of the actual
+                // hit location (and button ID), and enable it for dogfooders
+                // for a few days.  That'll give us a good idea of how close
+                // to the center of the button(s) most touch events are, to
+                // help us fine-tune the HIT_TARGET_PERCENT_* constants.
+
+                if (touchXPercent < X_HIT_MIN || touchXPercent > X_HIT_MAX
+                        || touchYPercent < Y_HIT_MIN || touchYPercent > Y_HIT_MAX) {
+                    // Missed!
+                    // if (DBG) log("  -> MISSED!");
+                    mDownEventHit = false;
+                    return true;  // Consume this event; don't let the button see it
+                } else {
+                    // Hit!
+                    // if (DBG) log("  -> HIT!");
+                    mDownEventHit = true;
+                    return false;  // Let this event through to the actual button
+                }
+            } else {
+                // This is a MOVE, UP or CANCEL event.
+                //
+                // We only do the "smaller hit target" check on DOWN events.
+                // For the subsequent MOVE/UP/CANCEL events, we let them
+                // through to the actual button IFF the previous DOWN event
+                // got through to the actual button (i.e. it was a "hit".)
+                return !mDownEventHit;
+            }
         }
     }
 
