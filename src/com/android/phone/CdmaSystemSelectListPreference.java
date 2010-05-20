181afad237a392dd4d179a6b82b4836a5537294c
Naveen Kalla <nkalla@codeaurora.org>
2010-05-20 09:26:44 -0700

    Add button to set CDMA subscription source.
    CDMA subscription source allows the user to choose between RUIM and NV
    modes for CDMA.
    
    Change-Id: I34809842318bfdd733ecad83209cd9e7b83c07be

--- src/com/android/phone/CdmaSystemSelectListPreference.java
+++ src/com/android/phone/CdmaSystemSelectListPreference.java
@@ -31,7 +31,7 @@ import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneFactory;
 import com.android.internal.telephony.TelephonyProperties;
 
-public class CdmaRoamingListPreference extends ListPreference {
+public class CdmaSystemSelectListPreference extends ListPreference {
 
     private static final String LOG_TAG = "CdmaRoamingListPreference";
     private static final boolean DBG = true;
@@ -39,7 +39,7 @@ public class CdmaRoamingListPreference extends ListPreference {
     private Phone mPhone;
     private MyHandler mHandler = new MyHandler();;
 
-    public CdmaRoamingListPreference(Context context, AttributeSet attrs) {
+    public CdmaSystemSelectListPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         mPhone = PhoneFactory.getDefaultPhone();
@@ -48,7 +48,7 @@ public class CdmaRoamingListPreference extends ListPreference {
                 mHandler.obtainMessage(MyHandler.MESSAGE_GET_ROAMING_PREFERENCE));
     }
 
-    public CdmaRoamingListPreference(Context context) {
+    public CdmaSystemSelectListPreference(Context context) {
         this(context, null);
     }
 
