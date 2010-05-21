8e382d750026f4519d0bc69326be1d845ff33bed
John Wang <johnwang@google.com>
2010-05-21 13:39:26 -0700

    Enforce GsmUmtsOptions only be called for GSM Phone.
    Change-Id: Iea7d1c6c8ae443aa58d5b3f282c8dc4b56df988f

--- src/com/android/phone/GsmUmtsOptions.java
+++ src/com/android/phone/GsmUmtsOptions.java
@@ -26,34 +26,28 @@ import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneFactory;
 
 /**
- * List of Network-specific settings screens.
+ * List of GSM/UMTS-specific network settings screens.
+ *
+ * This class should only be created for GSM/UMTS Phone
  */
 public class GsmUmtsOptions extends PreferenceActivity {
 
-    private PreferenceScreen mButtonAPNExpand;
-    private PreferenceScreen mButtonOperatorSelectionExpand;
-    private CheckBoxPreference mButtonPrefer2g;
-
-    private static final String BUTTON_APN_EXPAND_KEY = "button_apn_key";
-    private static final String BUTTON_OPERATOR_SELECTION_EXPAND_KEY = "button_carrier_sel_key";
     private static final String BUTTON_PREFER_2G_KEY = "button_prefer_2g_key";
 
 
     @Override
     protected void onCreate(Bundle icicle) {
+
+        // This class should only be created for GSM/UMTS Phone
+        if (PhoneFactory.getDefaultPhone().getPhoneType() != Phone.PHONE_TYPE_GSM) {
+            throw new RuntimeException("This should be called only for GSM/UMTS phone");
+        }
+
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.gsm_umts_options);
-        PreferenceScreen prefSet = getPreferenceScreen();
-        mButtonAPNExpand = (PreferenceScreen) prefSet.findPreference(BUTTON_APN_EXPAND_KEY);
-        mButtonOperatorSelectionExpand =
-                (PreferenceScreen) prefSet.findPreference(BUTTON_OPERATOR_SELECTION_EXPAND_KEY);
-        mButtonPrefer2g = (CheckBoxPreference) prefSet.findPreference(BUTTON_PREFER_2G_KEY);
-        if (PhoneFactory.getDefaultPhone().getPhoneType() != Phone.PHONE_TYPE_GSM) {
-            mButtonAPNExpand.setEnabled(false);
-            mButtonOperatorSelectionExpand.setEnabled(false);
-            mButtonPrefer2g.setEnabled(false);
-        }
+
+
     }
 
     @Override
