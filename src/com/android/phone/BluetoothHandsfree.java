91844242f671ff024755757dce97d1659fb5fce8
Herb Jellinek <Herb.Jellinek@gmail.com>
2010-08-12 14:16:51 -0700

    Event broadcasts should include the Bluetooth company ID.
    Change-Id: I969bbb4d963c4f257f5d122436a55add3de02c89

--- src/com/android/phone/BluetoothHandsfree.java
+++ src/com/android/phone/BluetoothHandsfree.java
@@ -1036,15 +1036,19 @@ public class BluetoothHandsfree {
     }
 
     /*
-     * Put the AT command, arguments, and device in an Intent and broadcast it.
+     * Put the AT command, company ID, arguments, and device in an Intent and broadcast it.
      */
     private void broadcastVendorSpecificEventIntent(String command,
+                                                    int companyID,
                                                     Object[] arguments,
                                                     BluetoothDevice device) {
         if (VDBG) log("broadcastVendorSpecificEventIntent(" + command + ")");
         Intent intent =
                 new Intent(BluetoothHeadset.ACTION_VENDOR_SPECIFIC_HEADSET_EVENT);
         intent.putExtra(BluetoothHeadset.EXTRA_VENDOR_SPECIFIC_HEADSET_EVENT_CMD, command);
+        intent.putExtra(BluetoothHeadset.EXTRA_VENDOR_SPECIFIC_HEADSET_EVENT_COMPANY_ID,
+                        companyID);
+
         // assert: all elements of args are Serializable
         intent.putExtra(BluetoothHeadset.EXTRA_VENDOR_SPECIFIC_HEADSET_EVENT_ARGS, arguments);
         intent.putExtra(BluetoothDevice.EXTRA_DEVICE, device);
@@ -1473,11 +1477,22 @@ public class BluetoothHandsfree {
      * @param commandName the name of the command.  For example, if the expected
      * incoming command is <code>AT+FOO=bar,baz</code>, the value of this should be
      * <code>"+FOO"</code>.
+     * @param companyID the Bluetooth SIG Company Identifier
      * @param parser the AtParser on which to register the command
      */
-    private void registerVendorSpecificCommand(String commandName, AtParser parser) {
-        parser.register(commandName, new VendorSpecificCommandHandler(commandName));
+    private void registerVendorSpecificCommand(String commandName,
+                                               int companyID,
+                                               AtParser parser) {
+        parser.register(commandName,
+                        new VendorSpecificCommandHandler(commandName, companyID));
     }
+
+    // Bluetooth SIG Company IDs for vendor-specific events
+
+    /*
+     * The Plantronics company ID.
+     */
+    private static final int PLANTRONICS_ID = 0x0055;
 
     /*
      * Register all vendor-specific commands here.
@@ -1486,7 +1501,7 @@ public class BluetoothHandsfree {
         AtParser parser = mHeadset.getAtParser();
 
         // Plantronics-specific headset events go here
-        registerVendorSpecificCommand("+XEVENT", parser);
+        registerVendorSpecificCommand("+XEVENT", PLANTRONICS_ID, parser);
     }
 
     /**
@@ -2254,13 +2269,17 @@ public class BluetoothHandsfree {
 
         private String mCommandName;
 
-        private VendorSpecificCommandHandler(String commandName) {
+        private int mCompanyID;
+
+        private VendorSpecificCommandHandler(String commandName, int companyID) {
             mCommandName = commandName;
+            mCompanyID = companyID;
         }
 
         @Override
         public AtCommandResult handleSetCommand(Object[] arguments) {
             broadcastVendorSpecificEventIntent(mCommandName,
+                                               mCompanyID,
                                                arguments,
                                                mHeadset.getRemoteDevice());
             return new AtCommandResult(AtCommandResult.OK);
