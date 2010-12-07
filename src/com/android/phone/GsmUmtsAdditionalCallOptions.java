9ebab0a5f2f7ee36a7695a3492817972c3fca3b6
Magnus Strandberg <magnus.strandberg@sonyericsson.com>
2010-12-07 14:48:41 +0100

    Implementing onResume in Additional call setting.
    Implementing onResume to handle initialization in
    GsmUmtsAdditionalCallOption since it extends
    TimeConsumingPreferenceActivity which relies on onResume
    to maintain its foreground state.
    TimeConsumingPreferenceActivity reading dialog will then
    block the settings for editing while reading the whole time.
    
    Change-Id: Id97ab6aaf857b08b8159afdc9c15732d71253670

--- src/com/android/phone/GsmUmtsAdditionalCallOptions.java
+++ src/com/android/phone/GsmUmtsAdditionalCallOptions.java
@@ -24,6 +24,9 @@ public class GsmUmtsAdditionalCallOptions extends
     private ArrayList<Preference> mPreferences = new ArrayList<Preference> ();
     private int mInitIndex= 0;
 
+    private boolean mFirstResume;
+    private Bundle mIcicle;
+
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -37,22 +40,37 @@ public class GsmUmtsAdditionalCallOptions extends
         mPreferences.add(mCLIRButton);
         mPreferences.add(mCWButton);
 
-        if (icicle == null) {
-            if (DBG) Log.d(LOG_TAG, "start to init ");
-            mCLIRButton.init(this, false);
-        } else {
-            if (DBG) Log.d(LOG_TAG, "restore stored states");
-            mInitIndex = mPreferences.size();
-            mCLIRButton.init(this, true);
-            mCWButton.init(this, true);
-            int[] clirArray = icicle.getIntArray(mCLIRButton.getKey());
-            if (clirArray != null) {
-                if (DBG) Log.d(LOG_TAG, "onCreate:  clirArray[0]="
-                        + clirArray[0] + ", clirArray[1]=" + clirArray[1]);
-                mCLIRButton.handleGetCLIRResult(clirArray);
-            } else {
+        // we wait to do the initialization until onResume so that the
+        // TimeConsumingPreferenceActivity dialog can display as it
+        // relies on onResume / onPause to maintain its foreground state.
+        mFirstResume = true;
+        mIcicle = icicle;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mFirstResume) {
+            if (mIcicle == null) {
+                if (DBG) Log.d(LOG_TAG, "start to init ");
                 mCLIRButton.init(this, false);
+            } else {
+                if (DBG) Log.d(LOG_TAG, "restore stored states");
+                mInitIndex = mPreferences.size();
+                mCLIRButton.init(this, true);
+                mCWButton.init(this, true);
+                int[] clirArray = mIcicle.getIntArray(mCLIRButton.getKey());
+                if (clirArray != null) {
+                    if (DBG) Log.d(LOG_TAG, "onCreate:  clirArray[0]="
+                            + clirArray[0] + ", clirArray[1]=" + clirArray[1]);
+                    mCLIRButton.handleGetCLIRResult(clirArray);
+                } else {
+                    mCLIRButton.init(this, false);
+                }
             }
+            mFirstResume = false;
+            mIcicle = null;
         }
 
         ActionBar actionBar = getActionBar();
