18a74c37a4674e55caba08885bd729bf2e12558f
Jiju Kinattingal <jijuk@codeaurora.org>
2010-12-27 13:30:02 +0530

    Phone: Basic PhoneApp changes for DSDS.
    Basic phone app changes to support dual sim dual standby(DSDS).
    
    Change-Id: Icffc8c0b167d19087374926233e6a121b0bdab94

---
+++ src/com/android/phone/SinglePhone.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2010, Code Aurora Forum. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.phone;
+
+import android.app.Activity;
+import android.app.Application;
+import android.app.ProgressDialog;
+import android.os.AsyncResult;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemProperties;
+import android.util.Log;
+
+import com.android.internal.telephony.IccCard;
+import com.android.internal.telephony.MmiCode;
+import com.android.internal.telephony.Phone;
+import com.android.internal.telephony.PhoneFactory;
+import com.android.phone.OtaUtils.CdmaOtaScreenState;
+
+
+
+public class SinglePhone {
+
+/* package */ static final String LOG_TAG = "SinglePhone";
+
+    /**
+     * SinglePhone -wide debug level:
+     *   0 - no debug logging
+     *   1 - normal debug logging if ro.debuggable is set (which is true in
+     *       "eng" and "userdebug" builds but not "user" builds)
+     *   2 - ultra-verbose debug logging
+     *
+     * Most individual classes in the phone app have a local DBG constant,
+     * typically set to
+     *   (SinglePhone.DBG_LEVEL >= 1) && (SystemProperties.getInt("ro.debuggable", 0) == 1)
+     * or else
+     *   (SinglePhone.DBG_LEVEL >= 2)
+     * depending on the desired verbosity.
+     */
+    /* package */ static final int DBG_LEVEL = 1;
+
+    private static final boolean DBG =
+            (SinglePhone.DBG_LEVEL >= 1) && (SystemProperties.getInt("ro.debuggable", 0) == 1);
+    private static final boolean VDBG = (SinglePhone.DBG_LEVEL >= 2);
+
+     // Message codes; see mHandler below.
+    private static final int EVENT_SIM_STATE_CHANGED = 8;
+    private static final int EVENT_UNSOL_CDMA_INFO_RECORD = 12;
+    private static final int EVENT_DOCK_STATE_CHANGED = 13;
+    private static final int EVENT_TECHNOLOGY_CHANGED = 17;
+
+    // The MMI codes are also used by the InCallScreen.
+    public static final int MMI_COMPLETE = 52;
+    public static final int MMI_CANCEL = 53;
+
+
+    public Phone mPhone;
+    public int mPhoneType;
+
+    //public SinglePhoneHandler mHandler; //handler for SinglePhone
+    public boolean mIsSimPinEnabled;
+    public String mCachedSimPin;
+    public boolean mIsSimPukLocked;
+    // Last phone state seen by updatePhoneState()
+    public Phone.State mLastPhoneState = Phone.State.IDLE;
+
+
+    // Internal SinglePhone cdma Call state tracker
+    public CdmaPhoneCallState mCdmaPhoneCallState = null;
+
+    // Following are the CDMA OTA information Objects used during OTA Call.
+    // cdmaOtaProvisionData object store static OTA information that needs
+    // to be maintained even during Slider open/close scenarios.
+    // cdmaOtaConfigData object stores configuration info to control visiblity
+    // of each OTA Screens.
+    // cdmaOtaScreenState object store OTA Screen State information.
+    public OtaUtils.CdmaOtaProvisionData mCdmaOtaProvisionData = null;
+    public OtaUtils.CdmaOtaConfigData mCdmaOtaConfigData = null;
+    public OtaUtils.CdmaOtaScreenState mCdmaOtaScreenState = null;
+    public OtaUtils.CdmaOtaInCallScreenUiState mCdmaOtaInCallScreenUiState = null;
+
+
+    SinglePhone(int subscription) {
+        if (VDBG) Log.d(LOG_TAG, "Single Phone constructor: "+ subscription);
+        // Get the phone
+        mPhone = PhoneFactory.getPhone(subscription);
+        mPhoneType = mPhone.getPhoneType();
+        //mHandler = new SinglePhoneHandler(mPhone);
+        //PhoneApp app = PhoneApp.getInstance();
+
+        // register for MMI/USSD
+        //mPhone.registerForMmiComplete(mHandler, MMI_COMPLETE, null);
+
+        // register connection tracking to PhoneUtils
+        //PhoneUtils.initializeConnectionHandler(mPhone);
+        // TODO: Register for Cdma Information Records
+        // phone.registerCdmaInformationRecord(mHandler, EVENT_UNSOL_CDMA_INFO_RECORD, null);
+
+        boolean phoneIsCdma = (mPhone.getPhoneType() == Phone.PHONE_TYPE_CDMA);
+
+        if (mPhone.getPhoneType() == Phone.PHONE_TYPE_CDMA) {
+            initializeCdmaVariables();
+        }
+
+    }
+
+    public void initializeCdmaVariables() {
+
+        if (mPhone.getPhoneType() == Phone.PHONE_TYPE_CDMA) {
+            // Create an instance of CdmaPhoneCallState and initialize it to IDLE
+            mCdmaPhoneCallState = new CdmaPhoneCallState();
+            mCdmaPhoneCallState.CdmaPhoneCallStateInit();
+
+            if (mCdmaOtaProvisionData == null) {
+                mCdmaOtaProvisionData = new OtaUtils.CdmaOtaProvisionData();
+            }
+            if (mCdmaOtaConfigData == null ) {
+                mCdmaOtaConfigData = new OtaUtils.CdmaOtaConfigData();
+            }
+            if (mCdmaOtaScreenState == null ) {
+                mCdmaOtaScreenState = new OtaUtils.CdmaOtaScreenState();
+            }
+            if (mCdmaOtaInCallScreenUiState == null) {
+                mCdmaOtaInCallScreenUiState = new OtaUtils.CdmaOtaInCallScreenUiState();
+            }
+        }
+    }
+
+    public void clearCdmaVariables() {
+
+        if (mPhone.getPhoneType() == Phone.PHONE_TYPE_CDMA) {
+            mCdmaPhoneCallState = null;
+            mCdmaOtaProvisionData = null;
+            mCdmaOtaConfigData = null;
+            mCdmaOtaScreenState = null;
+            mCdmaOtaInCallScreenUiState = null;
+        }
+    }
+};
