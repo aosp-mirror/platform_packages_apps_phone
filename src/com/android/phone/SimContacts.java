e73ef177d3c0fbfb9e353fadcf24b4600c36fb0a
Suresh Koleti <skolet@codeaurora.org>
2011-01-13 13:51:42 +0530

    packages/apps/Phone: Set initial focus for SimContacts.
    Unable to navigate the contacts stored in SIM card thru keypad due to there is no intial focus.
    Add mInitialSelection to zero, which is used to set the initial focus.
    
    Change-Id: I3855fed212c6aea282b40345e0c48c2ede2bccaf

--- src/com/android/phone/SimContacts.java
+++ src/com/android/phone/SimContacts.java
@@ -262,6 +262,8 @@ public class SimContacts extends ADNList {
         if (Intent.ACTION_PICK.equals(intent.getAction())) {
             // "index" is 1-based
             mInitialSelection = intent.getIntExtra("index", 0) - 1;
+        } else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            mInitialSelection = 0;
         }
         return intent.getData();
     }
