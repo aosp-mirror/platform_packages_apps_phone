aosp-mirror/platform_packages_apps_phone

commit 6d6523c51e68ddfd1140713ddd9cb279499e9f4f

Make the phone app call into the power manager for a short timeout when a call ends and we are still on the dialer screen.
Change-Id: Id80a8c50850a82b823c1ad31333f121d78781c01

src/com/android/phone/PhoneApp.java
@@ -422,8 +422,8 @@ public class PhoneApp extends Application implements AccelerometerListener.Orien
             // before registering for phone state changes
             PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
             mWakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK
-                    | PowerManager.ACQUIRE_CAUSES_WAKEUP
-                    | PowerManager.ON_AFTER_RELEASE, LOG_TAG);
+                    | PowerManager.ACQUIRE_CAUSES_WAKEUP,
+                    LOG_TAG);
             // lock used to keep the processor awake, when we don't care for the display.
             mPartialWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK
                     | PowerManager.ON_AFTER_RELEASE, LOG_TAG);
@@ -1661,5 +1661,14 @@ public class PhoneApp extends Application implements AccelerometerListener.Orien
                     + ar.exception);
         }
         phone.queryTTYMode(mHandler.obtainMessage(EVENT_TTY_MODE_GET));
+    }
+
+    /* package */ void clearUserActivityTimeout() {
+        try {
+            mPowerManagerService.clearUserActivityTimeout(SystemClock.uptimeMillis(),
+                    10*1000 /* 10 sec */);
+        } catch (RemoteException ex) {
+            // System process is dead.
+        }
     }
 }

src/com/android/phone/CallNotifier.java
@@ -959,6 +959,9 @@ public class CallNotifier extends Handler
             if (!mApplication.isShowingCallScreen()) {
                 if (VDBG) log("onDisconnect: force InCallScreen to finish()");
                 mApplication.dismissCallScreen();
+            } else {
+                if (VDBG) log("onDisconnect: In call screen. Set short timeout.");
+                mApplication.clearUserActivityTimeout();
             }
         }
 

commit 24830cc39835195358adfe0cde600db5186915ac

Fix for Screen Blank issue when accepting call.
This is fix the issue of screen going blank while the user
is in the process of accepting a call, caused by 6d6523c51.
Adding the flag ON_AFTER_RELEASE to reset user activity timer
when wake lock is released. That will keep the screen on for
a while before it goes to blank.

Change-Id: I6aef162d94bb557c045a268ec23760d887c52859

src/com/android/phone/PhoneApp.java
@@ -441,7 +441,8 @@ public class PhoneApp extends Application implements AccelerometerListener.Orien
             // before registering for phone state changes
             PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
             mWakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK
-                    | PowerManager.ACQUIRE_CAUSES_WAKEUP,
+                    | PowerManager.ACQUIRE_CAUSES_WAKEUP
+                    | PowerManager.ON_AFTER_RELEASE,
                     LOG_TAG);
             // lock used to keep the processor awake, when we don't care for the display.
             mPartialWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK
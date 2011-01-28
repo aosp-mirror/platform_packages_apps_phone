24830cc39835195358adfe0cde600db5186915ac
Rabendra Patel <rabendra.patel@sonyericsson.com>
2011-01-28 13:48:43 +0100

    Fix for Screen Blank issue when accepting call.
    This is fix the issue of screen going blank while the user
    is in the process of accepting a call, caused by 6d6523c51.
    Adding the flag ON_AFTER_RELEASE to reset user activity timer
    when wake lock is released. That will keep the screen on for
    a while before it goes to blank.
    
    Change-Id: I6aef162d94bb557c045a268ec23760d887c52859

--- src/com/android/phone/PhoneApp.java
+++ src/com/android/phone/PhoneApp.java
@@ -441,7 +441,8 @@ public class PhoneApp extends Application implements AccelerometerListener.Orien
             // before registering for phone state changes
             PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
             mWakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK
-                    | PowerManager.ACQUIRE_CAUSES_WAKEUP,
+                    | PowerManager.ACQUIRE_CAUSES_WAKEUP
+                    | PowerManager.ON_AFTER_RELEASE,
                     LOG_TAG);
             // lock used to keep the processor awake, when we don't care for the display.
             mPartialWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK
