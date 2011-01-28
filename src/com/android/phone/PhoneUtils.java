9166a0644e64c7110dac3945503b13c5be013fdd
Sang-Jun Park <sj2202.park@samsung.com>
2011-01-28 09:24:52 +0900

    "Connection problem or invalid MMI code" occrued when executing USSD USSD(Maxis *100#, Celcom *118#, Umobile *118#, Digi *128#)
    	When pressing touch key, USSD request is occured twice(UP/DOWN) respectively
    	So block a up key event
    
    Change-Id: Ie8c5f59bdc405048c2f941f44fa7db75cd69122e
    Signed-off-by: Sang-Jun Park <sj2202.park@samsung.com>

--- src/com/android/phone/PhoneUtils.java
+++ src/com/android/phone/PhoneUtils.java
@@ -1098,8 +1098,10 @@ public class PhoneUtils {
                             switch (keyCode) {
                                 case KeyEvent.KEYCODE_CALL:
                                 case KeyEvent.KEYCODE_ENTER:
-                                    phone.sendUssdResponse(inputText.getText().toString());
-                                    newDialog.dismiss();
+                                    if(event.getAction() == KeyEvent.ACTION_DOWN) {
+                                        phone.sendUssdResponse(inputText.getText().toString());
+                                        newDialog.dismiss();
+                                    }
                                     return true;
                             }
                             return false;
