5a96240723595956181a4ec3e9cb973d5612c1d9
Ramesh Sudini <ramesh.sudini@motorola.com>
2011-03-08 16:22:12 -0600

    Add LTE and eHRPD to PhoneInterfaceManager.
    Added HSPAP too.
    
    Change-Id: If3505092ccb850fb0b0e8c15bc276192639632d6

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -729,6 +729,12 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
                 return TelephonyManager.NETWORK_TYPE_EVDO_A;
             case ServiceState.RADIO_TECHNOLOGY_EVDO_B:
                 return TelephonyManager.NETWORK_TYPE_EVDO_B;
+            case ServiceState.RADIO_TECHNOLOGY_EHRPD:
+                return TelephonyManager.NETWORK_TYPE_EHRPD;
+            case ServiceState.RADIO_TECHNOLOGY_LTE:
+                return TelephonyManager.NETWORK_TYPE_LTE;
+            case ServiceState.RADIO_TECHNOLOGY_HSPAP:
+                return TelephonyManager.NETWORK_TYPE_HSPAP;
             default:
                 return TelephonyManager.NETWORK_TYPE_UNKNOWN;
         }
