f044d5a08e1981744e675ef9517a0efbd8b2c2a6
chen lixing 2 <lixing2.chen@sonyericsson.com>
2011-04-28 14:27:56 +0800

    Process dialog does not disappear after setting call forwarding.
    Dismiss the dialog not matter if it is on foreground.
    
    Change-Id: I4f1191675afa1b26e182d92be2a57ce161521151

--- src/com/android/phone/TimeConsumingPreferenceActivity.java
+++ src/com/android/phone/TimeConsumingPreferenceActivity.java
@@ -143,7 +143,7 @@ public class TimeConsumingPreferenceActivity extends PreferenceActivity
                 + ", reading=" + reading);
         mBusyList.remove(preference.getKey());
 
-        if (mBusyList.isEmpty() && mIsForeground) {
+        if (mBusyList.isEmpty()) {
             if (reading) {
                 dismissDialogSafely(BUSY_READING_DIALOG);
             } else {
