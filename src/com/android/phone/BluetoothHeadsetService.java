31d8a2e6052729250f9baddf7a31b357a59ffc69
Rafal Garbat <rafal.garbat@tieto.com>
2011-06-14 12:09:55 +0200

    Cancel inquiry before outgoing service connection.
    Before connecting to a headset, discovery needs to be
    canceled, to avoid "Connection timeout" error.
    
    Signed-off-by: Christian Bejram <christian.bejram@stericsson.com>
    Change-Id: I5bdbb99ed5deee316cd6fd04bc06c4a82cb6eacd

--- src/com/android/phone/BluetoothHeadsetService.java
+++ src/com/android/phone/BluetoothHeadsetService.java
@@ -564,6 +564,9 @@ public class BluetoothHeadsetService extends Service {
                 mRemoteHeadsets.get(device).mHeadsetType = type;
                 int channel = device.getServiceChannel(BluetoothUuid.Handsfree);
                 mConnectThread = new RfcommConnectThread(device, channel, type);
+                if (mAdapter.isDiscovering()) {
+                    mAdapter.cancelDiscovery();
+                }
                 mConnectThread.start();
                 if (getPriority(device) < BluetoothHeadset.PRIORITY_AUTO_CONNECT) {
                     setPriority(device, BluetoothHeadset.PRIORITY_AUTO_CONNECT);
@@ -576,6 +579,9 @@ public class BluetoothHeadsetService extends Service {
                 mRemoteHeadsets.get(device).mHeadsetType = type;
                 int channel = device.getServiceChannel(BluetoothUuid.HSP);
                 mConnectThread = new RfcommConnectThread(device, channel, type);
+                if (mAdapter.isDiscovering()) {
+                    mAdapter.cancelDiscovery();
+                }
                 mConnectThread.start();
                 if (getPriority(device) < BluetoothHeadset.PRIORITY_AUTO_CONNECT) {
                     setPriority(device, BluetoothHeadset.PRIORITY_AUTO_CONNECT);
