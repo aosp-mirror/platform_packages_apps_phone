adbef48a89cadd6448833bab47bdc3ddf2079e87
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange-ftgroup.com>
2011-06-28 14:08:13 +0200

    Fix issue 18054 http://code.google.com/p/android/issues/list?thanks=18054
    Change-Id: Id216f67f004740dc687d2552ee7e4cc364778ab0

--- src/com/android/phone/InCallScreen.java
+++ src/com/android/phone/InCallScreen.java
@@ -3476,18 +3476,8 @@ public class InCallScreen extends Activity
                 break;
 
             case DIALED_MMI:
-                // Our initial phone number was actually an MMI sequence.
-                // There's no real "error" here, but we do bring up the
-                // a Toast (as requested of the New UI paradigm).
-                //
-                // In-call MMIs do not trigger the normal MMI Initiate
-                // Notifications, so we should notify the user here.
-                // Otherwise, the code in PhoneUtils.java should handle
-                // user notifications in the form of Toasts or Dialogs.
-                if (mCM.getState() == Phone.State.OFFHOOK) {
-                    Toast.makeText(this, R.string.incall_status_dialed_mmi, Toast.LENGTH_SHORT)
-                        .show();
-                }
+                // The code in PhoneUtils.java handles user notification 
+                // in the form of a Dialog.
                 break;
 
             case CALL_FAILED:
