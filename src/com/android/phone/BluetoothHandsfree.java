aosp-mirror/platform_packages_apps_phone

commit 9760482b3e3f8a0c11cae2866ff03fab1b2c3242

Check virtual call status in terminateScoUsingVirtualVoiceCall
Check whether virtual call is in progress at the start of
terminateScoUsingVirtualVoiceCall. If not, we do no-op and return false.
This avoids turning off normal SCO channel that was carrying virtual
voice traffic.
bug 4364098

Change-Id: I08dbc86d7673f38c737e1786a2374b1bf698bc87

src/com/android/phone/BluetoothHandsfree.java
@@ -552,9 +552,11 @@ public class BluetoothHandsfree {
 
     /* package */ synchronized void disconnectHeadset() {
         audioOff();
-        if (isVirtualCallInProgress()) {
-            terminateScoUsingVirtualVoiceCall();
-        }
+
+        // No need to check if isVirtualCallInProgress()
+        // terminateScoUsingVirtualVoiceCall() does the check
+        terminateScoUsingVirtualVoiceCall();
+
         mHeadset = null;
         stopDebug();
         resetAtState();
@@ -2768,6 +2770,11 @@ public class BluetoothHandsfree {
 
     synchronized boolean terminateScoUsingVirtualVoiceCall() {
         if (DBG) log("terminateScoUsingVirtualVoiceCall: Received");
+
+        if (!isVirtualCallInProgress()) {
+            return false;
+        }
+
         // 1. Release audio connection
         audioOff();
 

commit 6e979d3698decc3631570a4f7db483cb94262c5d

Fix SCO audio APIs.
The original meaning of the virtual call variable was to
check if we have sent the CIEV commands to start the virtual call.
However, this meaning changed later on.

In accordance with the original use, the virtual call started variable
was set only when we were able to send CIEV commands. We can send those
commands only to handsfree devices and not to devices which support
just the headset profile. Some devices don't have telephony
support and hence we only support headset profile for such devices.

9760482b introduced the bug where we were checking this variable
during terminate but not setting it during initiation.

Change-Id: I67b870841cff2fd8d9765b4ae4ed8fb49a1d1da4

src/com/android/phone/BluetoothHandsfree.java
@@ -2842,9 +2842,10 @@ public class BluetoothHandsfree {
             result.addResponse("+CIEV: 2,1");
             result.addResponse("+CIEV: 3,0");
             sendURC(result.toString());
-            mVirtualCallStarted = true;
             if (DBG) Log.d(TAG, "initiateScoUsingVirtualVoiceCall: Sent Call-setup procedure");
         }
+
+        mVirtualCallStarted = true;
 
         // 3. Open the Audio Connection
         if (audioOn() == false) {
@@ -2871,14 +2872,14 @@ public class BluetoothHandsfree {
         audioOff();
 
         // 2. terminate call-setup
-        if (mBluetoothPhoneState.sendUpdate() && isVirtualCallInProgress()) {
+        if (mBluetoothPhoneState.sendUpdate()) {
             AtCommandResult result = new AtCommandResult(AtCommandResult.UNSOLICITED);
             // outgoing call
             result.addResponse("+CIEV: 2,0");
             sendURC(result.toString());
-            mVirtualCallStarted = false;
             if (DBG) log("terminateScoUsingVirtualVoiceCall: Sent Call-setup procedure");
         }
+        mVirtualCallStarted = false;
         mAudioPossible = false;
 
         // Done