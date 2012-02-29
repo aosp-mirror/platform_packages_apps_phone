56580aae12ddd490431fd8e2996092c66cc28127
Geremy Condra <gcondra@google.com>
2012-02-29 14:52:31 -0800

    Prevent DTMFTwelveKeyDialer from logging all keypresses.
    This fixes an externally reported problem in which keypresses made
    during phone calls were being sent to logcat.
    
    Change-Id: Ic47fdec0b600e7854933282b57693f143900a0ac

--- src/com/android/phone/DTMFTwelveKeyDialer.java
+++ src/com/android/phone/DTMFTwelveKeyDialer.java
@@ -1022,7 +1022,6 @@ public class DTMFTwelveKeyDialer implements
                 mDTMFQueue.add(new Character(dtmfDigit));
             } else {
                 String dtmfStr = Character.toString(dtmfDigit);
-                Log.i(LOG_TAG, "dtmfsent = " + dtmfStr);
                 mPhone.sendBurstDtmf(dtmfStr, 0, 0, mHandler.obtainMessage(DTMF_SEND_CNF));
                 // Set flag to indicate wait for Telephony confirmation.
                 mDTMFBurstCnfPending = true;
