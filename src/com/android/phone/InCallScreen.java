85a7c9b234e35d3f1272d423a6a5dc203958533b
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>
2012-04-04 16:55:45 +0200

    Fix issue #18054: USSD in-call toast
    Removes useless toast displayed when sending USSD during a call.
    
    http://code.google.com/p/android/issues/detail?id=18054
    
    Change-Id: Ic6ed1084558756987d9fdeaa43f7cdd76f3ea6e8

--- src/com/android/phone/InCallScreen.java
+++ src/com/android/phone/InCallScreen.java
@@ -3093,18 +3093,8 @@ public class InCallScreen extends Activity
                 break;
 
             case DIALED_MMI:
-                // Our initial phone number was actually an MMI sequence.
-                // There's no real "error" here, but we do bring up the
-                // a Toast (as requested of the New UI paradigm).
-                //
-                // In-call MMIs do not trigger the normal MMI Initiate
-                // Notifications, so we should notify the user here.
-                // Otherwise, the code in PhoneUtils.java should handle
-                // user notifications in the form of Toasts or Dialogs.
-                if (mCM.getState() == PhoneConstants.State.OFFHOOK) {
-                    Toast.makeText(mApp, R.string.incall_status_dialed_mmi, Toast.LENGTH_SHORT)
-                            .show();
-                }
+                // The code in PhoneUtils.java handles user notification
+                // in the form of a Dialog.
                 break;
 
             case CALL_FAILED:
