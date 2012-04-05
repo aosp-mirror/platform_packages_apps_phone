96ba22898f36bced10d7d65415de28d8e8ee013d
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>
2012-04-05 15:12:28 +0200

    USSD API
    This patch allows apps to send and receive USSD messages.
    
    USSD messages are sent using startService() with an intent created with
    "android.intent.action.ussd.SEND" as action and "tel:<MMI>" as URI, where
    <MMI> is the "number" of the USSD service requested.
    
    USSD messages are received using a BroadcastReceiver registered on
    intents having action "android.intent.action.ussd.RECEIVE". The actual USSD
    message is in extra Intent.EXTRA_TEXT ("android.intent.extra.TEXT").
    
    Remarks:
    
    The Phone application remains prioritary with respect to USSD. If it receives
    USSD when it is displayed, it will show the USSD message to the user. In this
    case no USSD receive intent will be broadcasted. Similarly, trying to send USSD
    from an app when the Phone app is displayed will failed, silently.
    
    All USSD messages received when the Phone app is not displayed are "cancelled"
    when broadcasted as intents (as if the user had pressed "cancel").
    
    Incoming USSD broadcast receivers will receive all USSD messages.
    
    Change-Id: I51a4e8b6753b31f7ceeb97145381ae819ce2adfe
    TODO: Expose and document USSD Intent actions in Intent.java
    TODO: Design an ad-hoc permission

---
+++ src/com/android/phone/UssdService.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2011-2012 France Telecom Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Author: Cuihtlauac ALVARADO
+ */
+
+package com.android.phone;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.telephony.PhoneNumberUtils;
+import com.android.internal.telephony.Phone;
+import com.android.internal.telephony.PhoneFactory;
+
+public class UssdService extends IntentService {
+
+  private Phone phone = null;
+  private PhoneApp phoneApp = null;
+
+  public UssdService() {
+    super("UssdService");
+    phoneApp = PhoneApp.getInstance();
+    phone = PhoneFactory.getDefaultPhone();
+    phoneApp.mCM.registerPhone(phone);
+  }
+
+  @Override
+  protected void onHandleIntent(Intent intent) {
+    String ussd = PhoneNumberUtils.getNumberFromIntent(intent, this);
+    if (!phoneApp.isShowingCallScreen()) {
+      phone.sendUssdResponse(ussd);
+    } else {
+      // TODO: How to inform the Intent sender no USSD was sent?
+    }
+  }
+
+}
