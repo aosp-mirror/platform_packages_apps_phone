93cc883fe8c25dd5af2036e8a16273326e798fae
pengli <liyuchenlp@gmail.com>
2012-06-28 15:05:35 +0800

    Phone: The waiting dialog always show when choose hide number in caller ID
    It should set mIsForeground true when start to init.
    
    Change-Id: I88d7c04cc8ccc56a996e9e9a89769dc0e4d6568b
    Signed-off-by: Charles Li<liyuchenlp@gmail.com>

--- src/com/android/phone/GsmUmtsAdditionalCallOptions.java
+++ src/com/android/phone/GsmUmtsAdditionalCallOptions.java
@@ -39,6 +39,7 @@ public class GsmUmtsAdditionalCallOptions extends
 
         if (icicle == null) {
             if (DBG) Log.d(LOG_TAG, "start to init ");
+            mIsForeground = true;
             mCLIRButton.init(this, false);
         } else {
             if (DBG) Log.d(LOG_TAG, "restore stored states");
