cdb16016c80c2075c7884341c24b6ac24114fc91
Banavathu, Srinivas Naik <snb@codeaurora.org>
2012-07-19 16:04:31 +0530

    Phone: Shows the list of the status PLMNs
    Shows all the list of the PLMNs available but does not
    highlight the status (unknown,available,current,forbidden)
    PLMNs amongst them.
    
    Shows the list of status (unknown,available,current,forbidden)
    PLMNs.
    
    Change-Id: If9986634116d3ebea8bf4a12882ac0b7adb8e420

--- src/com/android/phone/NetworkSetting.java
+++ src/com/android/phone/NetworkSetting.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * Copyright (c) 2012, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -407,7 +408,8 @@ public class NetworkSetting extends PreferenceActivity
                 // confusing mcc/mnc.
                 for (OperatorInfo ni : result) {
                     Preference carrier = new Preference(this, null);
-                    carrier.setTitle(getNetworkTitle(ni));
+                    carrier.setTitle(getNetworkTitle(ni) + "("
+                            + ni.getState().toString().toLowerCase() + ")");
                     carrier.setPersistent(false);
                     mNetworkList.addPreference(carrier);
                     mNetworkMap.put(carrier, ni);
