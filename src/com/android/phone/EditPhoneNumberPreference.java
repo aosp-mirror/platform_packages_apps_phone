c5c0a14a27ced2f41862597fc3cba1ce7a6c9832
Jason Chen <jasonchen@google.com>
2012-10-12 12:27:01 -0700

    Let's enhance
    Adding mipmap-* launcher icon assets. Cleaning up references to
    drawable based assets and removing unneeded files while we're
    at it.
    
    Bug: 6718166
    Change-Id: Ied65bb263728091667deb74966dc7d4adcc0eacf

--- src/com/android/phone/EditPhoneNumberPreference.java
+++ src/com/android/phone/EditPhoneNumberPreference.java
@@ -260,7 +260,7 @@ public class EditPhoneNumberPreference extends EditTextPreference {
             }
         }
         // set the call icon on the title.
-        builder.setIcon(R.drawable.ic_launcher_phone);
+        builder.setIcon(R.mipmap.ic_launcher_phone);
     }
 
 
