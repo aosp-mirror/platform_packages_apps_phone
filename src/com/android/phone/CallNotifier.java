b5fd503bc42369ca4fbf349f605587f5c58f5efd
Hariprasad Jayakumar <hjayak@codeaurora.org>
2013-01-08 19:08:29 -0800

    PhoneApp: Initialize ToneGenerator when needed
    Cherry pick from AOSP.
    
    Whenever there is a radio tech change, mSignalInfoToneGenerator
    is released and never created. This leads to phone app crash later
    when it is accessed for toneplay. This is fixed by not releasing the
    instance between RAT changes.
    
    Bug: 8609102
    Change-Id: I2f2513624b66892018dc3191daf3e48c63f744a1

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -208,18 +208,7 @@ public class CallNotifier extends Handler
 
         registerForNotifications();
 
-        // Instantiate the ToneGenerator for SignalInfo and CallWaiting
-        // TODO: We probably don't need the mSignalInfoToneGenerator instance
-        // around forever. Need to change it so as to create a ToneGenerator instance only
-        // when a tone is being played and releases it after its done playing.
-        try {
-            mSignalInfoToneGenerator = new ToneGenerator(AudioManager.STREAM_VOICE_CALL,
-                    TONE_RELATIVE_VOLUME_SIGNALINFO);
-        } catch (RuntimeException e) {
-            Log.w(LOG_TAG, "CallNotifier: Exception caught while creating " +
-                    "mSignalInfoToneGenerator: " + e);
-            mSignalInfoToneGenerator = null;
-        }
+        createSignalInfoToneGenerator();
 
         mRinger = ringer;
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
@@ -234,6 +223,26 @@ public class CallNotifier extends Handler
         telephonyManager.listen(mPhoneStateListener,
                 PhoneStateListener.LISTEN_MESSAGE_WAITING_INDICATOR
                 | PhoneStateListener.LISTEN_CALL_FORWARDING_INDICATOR);
+    }
+
+    private void createSignalInfoToneGenerator() {
+        // Instantiate the ToneGenerator for SignalInfo and CallWaiting
+        // TODO: We probably don't need the mSignalInfoToneGenerator instance
+        // around forever. Need to change it so as to create a ToneGenerator instance only
+        // when a tone is being played and releases it after its done playing.
+        if (mSignalInfoToneGenerator == null) {
+            try {
+                mSignalInfoToneGenerator = new ToneGenerator(AudioManager.STREAM_VOICE_CALL,
+                        TONE_RELATIVE_VOLUME_SIGNALINFO);
+                Log.d(LOG_TAG, "CallNotifier: mSignalInfoToneGenerator created when toneplay");
+            } catch (RuntimeException e) {
+                Log.w(LOG_TAG, "CallNotifier: Exception caught while creating " +
+                        "mSignalInfoToneGenerator: " + e);
+                mSignalInfoToneGenerator = null;
+            }
+        } else {
+            Log.d(LOG_TAG, "mSignalInfoToneGenerator created already, hence skipping");
+        }
     }
 
     @Override
@@ -868,11 +877,6 @@ public class CallNotifier extends Handler
         mCM.unregisterForCdmaOtaStatusChange(this);
         mCM.unregisterForRingbackTone(this);
         mCM.unregisterForResendIncallMute(this);
-
-        // Release the ToneGenerator used for playing SignalInfo and CallWaiting
-        if (mSignalInfoToneGenerator != null) {
-            mSignalInfoToneGenerator.release();
-        }
 
         // Clear ringback tone player
         mInCallRingbackTonePlayer = null;
